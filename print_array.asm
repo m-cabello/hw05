469e5d8868b75da6f9149c606fbfabeb3c7ec8f2
Martin Cabello <m_cabello@umail.ucsb.edu>
2021-02-10 13:43:34 -0800

    V2

--- print_array.asm
+++ print_array.asm
@@ -4,17 +4,35 @@
 # Don't forget to:
 #   make all arguments to any function go in $a0 and/or $a1
 #   make all returned values from functions go in $v0
-
 .data
 	# TODO: Write your initializations here
-
+    myArray: .word 1 2 3 4 5 6 7 8 9 10
+    answer: .asciiz "The contents of the array are:\n"
+    nline: .asciiz " "
 .text
 printA:
     # TODO: Write your function code here
-
+    bge $t0, 10, exit_printA
+    # load word and go to next one
+    lw $t2, 0($a1)
+    addi $a1, $a1, 4
+    # print value
+    li $v0, 1      
+    move $a0, $t2
+    syscall
+    # increment counter
+    addi $t0, $t0, 1
+    j printA
+exit_printA:
+    jr $ra
 main:
 	# TODO: Write your main function code here
-    
+    li $v0, 4
+    la $a0, answer
+    syscall
+    li $t0, 0
+    la $a1, myArray
+    jal printA
 exit:
 	# TODO: Write code to properly exit a SPIM simulation
     li $v0, 10
